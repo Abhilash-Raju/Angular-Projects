ca995261852504ccdd86d7198d36610537ee160e
Abhilash Raju <abhilash.raju8@gmail.com>
2023-02-17 23:34:45 +0530

    Component Creation

--- API-E-Commerce/src/app/app.module.ts
+++ API-E-Commerce/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadersComponent } from './components/headers/headers.component';
+import { HomeComponent } from './components/home/home.component';
+import { SellerComponent } from './components/seller/seller.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    HomeComponent,
+    SellerComponent,
+    CartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
