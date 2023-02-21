8a1fa5768aa1292a8949a5e1fe651a10e2eabc0a
Abhilash Raju <abhilash.raju8@gmail.com>
2023-02-21 23:47:45 +0530

    Component Addition

--- API-E-Commerce/src/app/app-routing.module.ts
+++ API-E-Commerce/src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
 import { HeadersComponent } from './components/headers/headers.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SellerComponent } from './components/seller/seller.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: HomeComponent,
+  },
+  {
+    path:'seller',
+    component:SellerComponent
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
   }
 ];
 
